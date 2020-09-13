c2e8ba66d0ad057714a222244b9f6cd29c8e8d44
dessale4 <dessale4@gmail.com>
2020-09-12 21:28:38 -0500

    first commit

---
+++ src/test/java/com/desale/dockerJenkinsIntegration/DockerJenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desale.dockerJenkinsIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
